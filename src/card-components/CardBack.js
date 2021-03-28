53aa9a31e28b082093084a9a1121ce85db596acb
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-28 00:51:26 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -25,7 +25,7 @@ export default class CardBack extends Component {
         <h3 className="title"></h3>
         <span />
         { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
-        
+        { this.generateRatingElement() }
         <span />
         <h5 className="genres"></h5>
       </div>
