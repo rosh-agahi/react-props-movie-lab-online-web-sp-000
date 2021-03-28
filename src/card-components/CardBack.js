2c685fc2125495e42968c75ad281ed6fffab2936
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-28 00:50:52 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,6 +12,11 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating !== null){
+      return <img src = {imgMapper[this.props.IMDBRating]} alt=""/>
+    } else{
+      return <h4>No Rating Found</h4>
+    }
   }
 
   render() {
